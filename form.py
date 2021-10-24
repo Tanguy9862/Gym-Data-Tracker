0fc7eda7880a3ea340be8c4a0315257536ead176
hideky <hideky98@gmail.com>
2021-10-24 23:13:57 +0200

    Add routes (track_rm, edit_rm, settings)

--- form.py
+++ form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, FloatField, DateField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, FloatField, DateField, RadioField
 from wtforms.validators import DataRequired, Length, Email, NumberRange
 import datetime
 
@@ -14,10 +14,11 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email(message="Veuillez entrer un email valide.")])
+    username = StringField("Pseudonyme", validators=[DataRequired()])
     password = PasswordField("Mot de passe", validators=[DataRequired(), Length(min=8, max=15,
                                                                             message="Le mot de passe doit comporter "
                                                                                     "entre 8 et 15 caractères")])
-    username = StringField("Pseudonyme", validators=[DataRequired()])
+    sex = RadioField("Genre", validators=[DataRequired()], choices=["Masculin", "Féminin"])
     register_submit = SubmitField("Inscription")
 
 
@@ -50,4 +51,16 @@ class EditWorkout(FlaskForm):
 class SearchUser(FlaskForm):
     search_field = StringField("Pseudonyme :", validators=[DataRequired()])
     search_submit = SubmitField("Rechercher")
+
+
+class AddPersonalRecord(FlaskForm):
+    date_record = DateField("Date(JJ-MM-AAAA)", format='%d-%m-%Y', default=datetime.date.today(), validators=[DataRequired()])
+    input_record = StringField("Nouveau record(kg)", validators=[DataRequired()])
+    save_submit = SubmitField("Enregistrer")
+
+
+class EditBodyWeight(FlaskForm):
+    date_bw = DateField("Date(JJ-MM-AAAA)", format='%d-%m-%Y', default=datetime.date.today(), validators=[DataRequired()])
+    input_bw = StringField("Poids de corps (kg)", validators=[DataRequired()])
+    save_submit = SubmitField("Enregistrer")
 
