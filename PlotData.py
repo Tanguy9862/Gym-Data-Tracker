a2e745195c5360029d48d39cfdbfcf67d05e42bb
hideky <hideky98@gmail.com>
2021-10-24 04:46:05 +0200

    Add new method to make line plot

--- PlotData.py
+++ PlotData.py
@@ -1,8 +1,33 @@
+import plotly
 import plotly.express as px
+import plotly.graph_objs as go
+import json
+import pandas as pd
 
 
 class Plot:
 
-    def scatter_plot(self, df, x_data, y_data, color):
-        fig = px.scatter(df, x=x_data, y=y_data, color=color)
-        fig.show()
+    def __init__(self):
+        pass
+
+    def line_plot(self, df_exercise, title):
+
+        print(df_exercise.dtypes)
+        print(df_exercise)
+
+        df_exercise = df_exercise.sort_values(by='Date', ascending=False)  # à modifier
+        fig = px.line(
+            df_exercise,
+            x='Date',
+            y='Charge',
+            color='Répétitions',
+            markers=True,
+            width=900,
+            height=500
+        )
+        fig.update_layout(
+            title=title,
+            yaxis_title="Charge (en Kg)",
+        )
+        return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
